0b95a8b05b099c0bfbf8dae308016cb688a31398
Robert Lowe <robert@iblargz.com>
2016-05-03 14:24:45 -0400

    Update phantomjs.sh

--- extra/phantomjs.sh
+++ extra/phantomjs.sh
@@ -6,15 +6,15 @@ ARCHIVE_NAME=phantomjs-${VERSION}-linux-x86_64
 FILE_NAME=${ARCHIVE_NAME}.tar.bz2
 BUILDPACK_PHANTOMJS_PACKAGE=https://bitbucket.org/ariya/phantomjs/downloads/${FILE_NAME}
 
-ARCHIVE_DIR  = $APP_CHECKOUT_DIR/.phantomjs
-ARCHIVE_FILE = $ARCHIVE_DIR/$FILE_NAME
+ARCHIVE_DIR=$APP_CHECKOUT_DIR/.phantomjs
+ARCHIVE_FILE=$ARCHIVE_DIR/$FILE_NAME
 mkdir -p $ARCHIVE_DIR
 if ! [ -e $ARCHIVE_FILE ]; then
   echo "-----> Fetching PhantomJS ${VERSION} binaries from ${BUILDPACK_PHANTOMJS_PACKAGE} to ${ARCHIVE_FILE}"
   curl $BUILDPACK_PHANTOMJS_PACKAGE -L -s -o $ARCHIVE_FILE
 fi
 
-DUMP_DIR = $ARCHIVE_DIR/extract
+DUMP_DIR=$ARCHIVE_DIR/extract
 mkdir -p $DUMP_DIR
 if ! [ -e $DUMP_DIR ]; then
   echo "-----> Extracting PhantomJS ${VERSION} binaries to ${DUMP_DIR}"
